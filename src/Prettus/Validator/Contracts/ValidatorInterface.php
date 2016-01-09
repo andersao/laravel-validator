0fb445d04cb6d421761c8f47623115251b24097a
Phan Thanh Cong <ptcong90@gmail.com>
2016-01-09 14:19:24 +0700

    CS fixes with addition methods

--- src/Prettus/Validator/Contracts/ValidatorInterface.php
+++ src/Prettus/Validator/Contracts/ValidatorInterface.php
@@ -4,76 +4,94 @@ use Illuminate\Contracts\Support\MessageBag;
 use Prettus\Validator\Exceptions\ValidatorException;
 
 /**
- * Interface ValidatorInterface
+ * Interface ValidatorInterface.
+ *
  * @package Prettus\Validator\Contracts
  */
-interface ValidatorInterface {
-
+interface ValidatorInterface
+{
     const RULE_CREATE = 'create';
     const RULE_UPDATE = 'update';
 
     /**
-     * Set Id
+     * Set Id.
      *
      * @param $id
+     *
      * @return $this
      */
     public function setId($id);
 
     /**
-     * With
+     * With.
      *
      * @param array
+     *
      * @return $this
      */
     public function with(array $input);
 
     /**
-     * Pass the data and the rules to the validator
+     * Returns value of specific data key.
+     *
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function getData($key, $default = null);
+
+    /**
+     * Pass the data and the rules to the validator.
      *
      * @param string $action
-     * @return boolean
+     *
+     * @return bool
      */
-    public function passes( $action = null );
+    public function passes($action = null);
 
 
     /**
-     * Pass the data and the rules to the validator or throws ValidatorException
+     * Pass the data and the rules to the validator or throws ValidatorException.
      *
      * @throws ValidatorException
+     *
      * @param string $action
-     * @return boolean
+     *
+     * @return bool
      */
-    public function passesOrFail( $action = null );
+    public function passesOrFail($action = null);
 
     /**
-     * Errors
+     * Errors.
      *
      * @return array
      */
     public function errors();
 
     /**
-     * Errors
+     * Errors.
      *
      * @return MessageBag
      */
     public function errorsBag();
 
     /**
-     * Set Rules for Validation
+     * Set Rules for Validation.
      *
      * @param array $rules
+     *
      * @return $this
      */
     public function setRules(array $rules);
 
     /**
-     * Get rule for validation by action ValidatorInterface::RULE_CREATE or ValidatorInterface::RULE_UPDATE
+     * Get rule for validation by action ValidatorInterface::RULE_CREATE or ValidatorInterface::RULE_UPDATE.
      *
      * Default rule: ValidatorInterface::RULE_CREATE
      *
      * @param $action
+     *
      * @return array
      */
     public function getRules($action = null);
--- src/Prettus/Validator/Exceptions/ValidatorException.php
+++ src/Prettus/Validator/Exceptions/ValidatorException.php
@@ -5,11 +5,12 @@ use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Support\MessageBag;
 
 /**
- * Class ValidatorException
+ * Class ValidatorException.
+ *
  * @package Prettus\Validator\Exceptions
  */
-class ValidatorException extends \Exception implements Jsonable, Arrayable {
-
+class ValidatorException extends \Exception implements Jsonable, Arrayable
+{
     /**
      * @var MessageBag
      */
@@ -18,15 +19,37 @@ class ValidatorException extends \Exception implements Jsonable, Arrayable {
     /**
      * @param MessageBag $messageBag
      */
-    public function __construct(MessageBag $messageBag){
+    public function __construct(MessageBag $messageBag)
+    {
         $this->messageBag = $messageBag;
     }
 
     /**
      * @return MessageBag
      */
-    public function getMessageBag(){
+    public function getMessageBag()
+    {
         return $this->messageBag;
+    }
+
+    /**
+     * Get all messages separated by comma.
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return implode('. ', $this->getMessages());
+    }
+
+    /**
+     * Returns all messages.
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        return $this->getMessageBag()->all();
     }
 
     /**
@@ -37,20 +60,20 @@ class ValidatorException extends \Exception implements Jsonable, Arrayable {
     public function toArray()
     {
         return [
-            'error'=>'validation_exception',
-            'error_description'=>$this->getMessageBag()
+            'error'             => 'validation_exception',
+            'error_description' => $this->getMessageBag()
         ];
     }
 
     /**
      * Convert the object to its JSON representation.
      *
-     * @param  int $options
+     * @param int $options
+     *
      * @return string
      */
     public function toJson($options = 0)
     {
         return json_encode($this->toArray(), $options);
     }
-
 }
